35fa2aa28a3c88406fa6af6180a00fc739005331
Kevin Nikolai <nikolai10052005@gmail.com>
2024-07-22 17:56:05 -0300

    feat: header notification bell

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,20 @@
 import { View, Pressable, Text } from "react-native";
-import { Ionicons, Feather } from '@expo/vector-icons'
+import { Feather } from '@expo/vector-icons'
 
 
 export function Header(){
     return (
-        <View>
-            <Pressable>
-                <Ionicons name="menu" size={20} color="#121212"/>
+        <View className="w-full flex flex-row items-center justify-between">
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Feather name="menu" size={20} color="#121212"/>
             </Pressable>
+
+
+            <Pressable className="w-10 h-10 bg-white rounded-full flex justify-center items-center">
+                <Feather name="bell" size={20} color="#121212"/>
+            </Pressable>
+
+
         </View>       
     );
 }
